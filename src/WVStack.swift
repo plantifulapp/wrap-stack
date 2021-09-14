c57206c7bb899f7c502406de51b6c56726140752
Andrei <andrei@andreimatei.ro>
2021-09-14 20:42:58 +0100

    temp comment out Previews in WVStack.swift

--- src/WVStack.swift
+++ src/WVStack.swift
@@ -55,167 +55,167 @@ public struct WVStack: View {
   }
 }
 
-struct WVStack_Previews: PreviewProvider {
-  
-  @ViewArrayBuilder static func single() -> [AnyView] {
-    Color.purple.frame(width: 50, height: 50)
-  }
-  
-  static var previews: some View {
-    Group {
-      
-      WVStack(alignment: .center, spacing: 10) {[
-        Color.red.frame(width: 150, height: 100),
-        Color.green.frame(width: 50, height: 150),
-        Color.yellow.frame(width: 100, height: 150),
-        Color.orange.frame(width: 50, height: 50),
-        Color.purple.frame(width: 30, height: 50),
-        Color.blue.frame(width: 50, height: 50),
-        Color.gray.frame(width: 50, height: 50),
-        Color.green.frame(width: 50, height: 50),
-        Color.yellow.frame(width: 100, height: 150),
-        Color.orange.frame(width: 50, height: 50),
-        Color.purple.frame(width: 30, height: 50),
-        Color.red.frame(width: 150, height: 100),
-        Color.green.frame(width: 50, height: 150),
-        Color.yellow.frame(width: 100, height: 150),
-        Color.orange.frame(width: 50, height: 50),
-        Color.purple.frame(width: 30, height: 50),
-        Color.blue.frame(width: 50, height: 50),
-        Color.gray.frame(width: 50, height: 50),
-        Color.green.frame(width: 50, height: 50),
-        Color.blue.frame(width: 50, height: 50),
-        Color.gray.frame(width: 50, height: 50),
-        Color.green.frame(width: 50, height: 50),
-      ]}
-      //.frame(height: 350)
-      .border(Color.black, width: 1)
-
-      ScrollView {
-        VStack {
-          Group {
-            Text("Header")
-            Spacer()
-          }
-          Group {
-            WVStack { () -> [AnyView] in }
-
-            // FIXME: https://bugs.swift.org/browse/SR-11628
-            WVStack {
-              Color.red
-            }
-          }
-          Group {
-            
-            // FIXME: https://bugs.swift.org/browse/SR-11628
-            WVStack(content: single)
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-              Color.purple.frame(width: 50, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-              Color.purple.frame(width: 50, height: 50)
-              Color.blue.frame(width: 50, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-              Color.purple.frame(width: 50, height: 50)
-              Color.blue.frame(width: 50, height: 50)
-              Color.black.frame(width: 50, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-              Color.purple.frame(width: 50, height: 50)
-              Color.blue.frame(width: 50, height: 50)
-              Color.black.frame(width: 50, height: 50)
-              Color.pink.frame(width: 50, height: 50)
-            }
-            
-            WVStack {
-              Color.red
-              Color.gray.frame(width: 80, height: 50)
-              Color.green.frame(width: 150, height: 30)
-              Color.yellow.frame(width: 150, height: 70)
-              Color.orange.frame(width: 50, height: 50)
-              Color.purple.frame(width: 50, height: 50)
-              Color.blue.frame(width: 50, height: 50)
-              Color.black.frame(width: 50, height: 50)
-              Color.pink.frame(width: 50, height: 50)
-              Color.white.frame(width: 50, height: 50)
-            }
-          }
-          
-          
-          Group {
-            WVStack(alignment: .trailing, spacing: 10) {[
-              Color.red.frame(width: 100, height: 50),
-              Color.gray.frame(width: 80, height: 50),
-              Color.green.frame(width: 150, height: 30),
-              Color.yellow.frame(width: 150, height: 70),
-              Color.orange.frame(width: 50, height: 50),
-              Color.purple.frame(width: 50, height: 50),
-              Color.blue.frame(width: 50, height: 50),
-              Color.black.frame(width: 50, height: 50),
-              Color.pink.frame(width: 50, height: 50),
-              Color.white.frame(width: 50, height: 50),
-              ]}
-              .frame(width: 350)
-              .border(Color.black, width: 1)
-          }
-          Group {
-            Spacer()
-            Text("Footer")
-          }
-        }
-      }
-
-    }
-  }
-}
+//struct WVStack_Previews: PreviewProvider {
+//
+//  @ViewArrayBuilder static func single() -> [AnyView] {
+//    Color.purple.frame(width: 50, height: 50)
+//  }
+//
+//  static var previews: some View {
+//    Group {
+//
+//      WVStack(alignment: .center, spacing: 10) {[
+//        Color.red.frame(width: 150, height: 100),
+//        Color.green.frame(width: 50, height: 150),
+//        Color.yellow.frame(width: 100, height: 150),
+//        Color.orange.frame(width: 50, height: 50),
+//        Color.purple.frame(width: 30, height: 50),
+//        Color.blue.frame(width: 50, height: 50),
+//        Color.gray.frame(width: 50, height: 50),
+//        Color.green.frame(width: 50, height: 50),
+//        Color.yellow.frame(width: 100, height: 150),
+//        Color.orange.frame(width: 50, height: 50),
+//        Color.purple.frame(width: 30, height: 50),
+//        Color.red.frame(width: 150, height: 100),
+//        Color.green.frame(width: 50, height: 150),
+//        Color.yellow.frame(width: 100, height: 150),
+//        Color.orange.frame(width: 50, height: 50),
+//        Color.purple.frame(width: 30, height: 50),
+//        Color.blue.frame(width: 50, height: 50),
+//        Color.gray.frame(width: 50, height: 50),
+//        Color.green.frame(width: 50, height: 50),
+//        Color.blue.frame(width: 50, height: 50),
+//        Color.gray.frame(width: 50, height: 50),
+//        Color.green.frame(width: 50, height: 50),
+//      ]}
+//      //.frame(height: 350)
+//      .border(Color.black, width: 1)
+//
+//      ScrollView {
+//        VStack {
+//          Group {
+//            Text("Header")
+//            Spacer()
+//          }
+//          Group {
+//            WVStack { () -> [AnyView] in }
+//
+//            // FIXME: https://bugs.swift.org/browse/SR-11628
+//            WVStack {
+//              Color.red
+//            }
+//          }
+//          Group {
+//
+//            // FIXME: https://bugs.swift.org/browse/SR-11628
+//            WVStack(content: single)
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//              Color.purple.frame(width: 50, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//              Color.purple.frame(width: 50, height: 50)
+//              Color.blue.frame(width: 50, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//              Color.purple.frame(width: 50, height: 50)
+//              Color.blue.frame(width: 50, height: 50)
+//              Color.black.frame(width: 50, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//              Color.purple.frame(width: 50, height: 50)
+//              Color.blue.frame(width: 50, height: 50)
+//              Color.black.frame(width: 50, height: 50)
+//              Color.pink.frame(width: 50, height: 50)
+//            }
+//
+//            WVStack {
+//              Color.red
+//              Color.gray.frame(width: 80, height: 50)
+//              Color.green.frame(width: 150, height: 30)
+//              Color.yellow.frame(width: 150, height: 70)
+//              Color.orange.frame(width: 50, height: 50)
+//              Color.purple.frame(width: 50, height: 50)
+//              Color.blue.frame(width: 50, height: 50)
+//              Color.black.frame(width: 50, height: 50)
+//              Color.pink.frame(width: 50, height: 50)
+//              Color.white.frame(width: 50, height: 50)
+//            }
+//          }
+//
+//
+//          Group {
+//            WVStack(alignment: .trailing, spacing: 10) {[
+//              Color.red.frame(width: 100, height: 50),
+//              Color.gray.frame(width: 80, height: 50),
+//              Color.green.frame(width: 150, height: 30),
+//              Color.yellow.frame(width: 150, height: 70),
+//              Color.orange.frame(width: 50, height: 50),
+//              Color.purple.frame(width: 50, height: 50),
+//              Color.blue.frame(width: 50, height: 50),
+//              Color.black.frame(width: 50, height: 50),
+//              Color.pink.frame(width: 50, height: 50),
+//              Color.white.frame(width: 50, height: 50),
+//              ]}
+//              .frame(width: 350)
+//              .border(Color.black, width: 1)
+//          }
+//          Group {
+//            Spacer()
+//            Text("Footer")
+//          }
+//        }
+//      }
+//
+//    }
+//  }
+//}
